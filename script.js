eb9b3af6df2a99621e42126c9c99503a3bfbbb02
imhul <blashirk@gmail.com>
2016-02-03 18:34:59 +0200

    homework 1

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-for (var i = 0; i < 36; i++) {
-	console.log(i);
+
+var month = prompt('Введите месяц вашего рождения в числовом формате');
+var day = prompt('Введите день вашего рождения в числовом формате');
+
+if ((day >= 20 && month == 1) || (day < 18 && month == 2)) {
+alert('Водолей');
+}
+else if ((day >= 20 && month == 2) || (day < 18 && month == 3)) {
+alert('Рыбы');
+}
+else if ((day >= 20 && month == 3) || (day < 18 && month == 4)) {
+alert('Овен');
+}
+else if ((day >= 20 && month == 4) || (day < 18 && month == 5)) {
+alert('Телец');
+}
+else if ((day >= 20 && month == 5) || (day < 18 && month == 6)) {
+alert('Близнецы');
+}
+else if ((day >= 20 && month == 6) || (day < 18 && month == 7)) {
+alert('Рак');
+}
+else if ((day >= 20 && month == 7) || (day < 18 && month == 8)) {
+alert('Лев');
+}
+else if ((day >= 20 && month == 8) || (day < 18 && month == 9)) {
+alert('Дева');
+}
+else if ((day >= 20 && month == 9) || (day < 18 && month == 10)) {
+alert('Весы');
+}
+else if ((day >= 20 && month == 10) || (day < 18 && month == 11)) {
+alert('Скорпион');
+}
+else if ((day >= 20 && month == 11) || (day < 18 && month == 12)) {
+alert('Стрелец');
+}
+else {
+alert('Козерог');
 }
---
+++ toFixed.js
@@ -0,0 +1,3 @@
+var value = prompt('Введите гривны');
+value = value/25;
+alert('У вас'+value.toFixed(2)+'у.е.');
