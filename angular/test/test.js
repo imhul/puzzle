2a63f4d3e698af28a22a39d34270c3e13512f49c
imhul <blashirk@gmail.com>
2016-04-20 18:53:50 +0300

    renew

--- angular/test/test.js
+++ angular/test/test.js
@@ -1,3 +1,48 @@
+describe("pow", function() {
+
+  describe("возводит x в степень n", function() {
+
+    function makeTest(x) {
+      var expected = x * x * x;
+      it("при возведении " + x + " в степень 3 результат: " + expected, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (var x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+
+  });
+
+  it("при возведении в отрицательную степень результат NaN", function() {
+    assert(isNaN(pow(2, -1)), "pow(2, -1) не NaN");
+  });
+
+  it("при возведении в дробную степень результат NaN", function() {
+    assert(isNaN(pow(2, 1.5)), "pow(2, -1.5) не NaN");
+  });
+
+  describe("любое число, кроме нуля, в степени 0 равно 1", function() {
+
+    function makeTest(x) {
+      it("при возведении " + x + " в степень 0 результат: 1", function() {
+        assert.equal(pow(x, 0), 1);
+      });
+    }
+
+    for (var x = -5; x <= 5; x += 2) {
+      makeTest(x);
+    }
+
+  });
+
+  it("ноль в нулевой степени даёт NaN", function() {
+    assert(isNaN(pow(0, 0)), "0 в степени 0 не NaN");
+  });
+
+});
+
 // var should = require('chai').should();
 //
 // describe('Array', function() {
@@ -8,14 +53,14 @@
 //         });
 //     });
 // });
-
-chai.should();
-
-describe('Array', function() {
-    describe('method indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            [1,2,3].indexOf(5).should.equal(-1);
-            [1,2,3].indexOf(0).should.equal(-1);
-        });
-    });
-});
+/////////////////////////////////////////////////////////////////////////////
+// chai.should();
+//
+// describe('Array', function() {
+//     describe('method indexOf()', function () {
+//         it('should return -1 when the value is not present', function () {
+//             [1,2,3].indexOf(5).should.equal(-1);
+//             [1,2,3].indexOf(0).should.equal(-1);
+//         });
+//     });
+// });
