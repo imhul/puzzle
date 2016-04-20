b6ab7480fc6189362f18aa62601fcd0af6e7833b
imhul <blashirk@gmail.com>
2016-04-20 18:57:14 +0300

    renew

--- angular/app/main.js
+++
@@ -1,5 +0,0 @@
-(function(app) {
-    document.addEventListener('DOMContentLoaded', () => {
-        ng.platform.browser.bootstrap(app.AppComponent);
-    });
-})(window.app || (window.app = {}));
--- info/js/toFixed.js
+++
@@ -1,3 +0,0 @@
-var value = prompt('Введите гривны');
-value = value/25;
-alert('У вас'+value.toFixed(2)+'у.е.');
